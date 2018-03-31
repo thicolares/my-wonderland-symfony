f573fb0fb9b1f2798c740c7c02a40b6b762aed1a
Thiago Colares <thicolares@gmail.com>
2018-03-31 16:59:19 -0300

    Request refresh and access Spotify's tokens

--- src/controller/SpotifyAuth.php
+++ src/controller/SpotifyAuth.php
@@ -1,5 +1,6 @@
 <?php
 namespace MyWonderland\Controller;
+use GuzzleHttp\Client;
 
 /**
  * Created by PhpStorm.
@@ -10,13 +11,15 @@ namespace MyWonderland\Controller;
 
 class SpotifyAuth extends AbstractController
 {
+    const REDIRECT_URI = 'http://my-wonderland.localhost/callback'; // @todo extract the base
+
     public function auth() {
         $baseUri = 'https://accounts.spotify.com/authorize/';
         $scopes = 'user-read-private user-read-email';
-        $redirectUri = 'http://my-wonderland.localhost/callback';
+
         $uri = '?client_id=' . getenv('SPOTIFY_CLIENT_ID') .
             '&response_type=code' .
-            '&redirect_uri=' . rawurlencode($redirectUri) .
+            '&redirect_uri=' . rawurlencode(self::REDIRECT_URI) .
             ($scopes ? '&scope=' . rawurlencode($scopes) : '') .
             '&state=' . getenv('SPOTIFY_CALLBACK_STATE');
         header('Location: ' . $baseUri . $uri);
@@ -24,9 +27,27 @@ class SpotifyAuth extends AbstractController
 
     public function callback($code, $state = null) {
         print "authCallback";
-        print_r($_GET);
+
         var_dump($code);
         var_dump($state);
         var_dump(getenv('SPOTIFY_CALLBACK_STATE'));
+
+        $client = new Client();
+        $response = $client->request('POST', 'https://accounts.spotify.com/api/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'code' => $code,
+                'redirect_uri' => self::REDIRECT_URI
+            ],
+            'headers'  => [
+                'Authorization' => 'Basic  ' .
+                    base64_encode(getenv('SPOTIFY_CLIENT_ID') . ':' . getenv('SPOTIFY_CLIENT_SECRET'))
+            ]
+        ]);
+
+        print_r(\json_decode($response->getBody()->getContents(), true));
+
+
+
     }
 }
